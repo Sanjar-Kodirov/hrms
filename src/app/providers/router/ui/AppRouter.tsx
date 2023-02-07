08efcc8859b27ebbca8ef43a721f30bcbb54f13e
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-02-07 21:50:30 +0500

    Navbar. Шаблоны для разработки.

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -10,8 +10,6 @@ const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        {/* <Route path={"/about"} element={<AboutPage />} />
-        <Route path={"/"} element={<MainPage />} /> */}
         {Object.values(routeConfig).map(({ path, element }) => (
           <Route key={path} path={path} element={element} />
         ))}
