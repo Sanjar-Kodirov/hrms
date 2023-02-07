aac4db5cd3711a19e88ddf871b844605d163a3fc
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-02-07 15:50:33 +0500

    App router config

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,23 @@
+import AboutPage from "pages/AboutPage/ui/AboutPage";
+import MainPage from "pages/MainPage/ui/MainPage";
+import { Suspense } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+const AppRouter = () => {
+  console.log(routeConfig);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {/* <Route path={"/about"} element={<AboutPage />} />
+        <Route path={"/"} element={<MainPage />} /> */}
+        {Object.values(routeConfig).map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
