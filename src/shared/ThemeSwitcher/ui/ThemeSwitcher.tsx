e1b2795f9029eeceb4168bf83f9f830b7ed470ec
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-02-11 20:33:42 +0500

    add stylelint, i18n plugin for eslint, fix all styles

--- src/shared/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,13 +2,12 @@ import { useTheme, Theme } from 'app/providers/ThemeProvider';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
 
 export const ThemeSwitcher = () => {
     const { theme, toggleTheme } = useTheme();
     return (
-        <div className={cls.ThemeSwitcher}>
-            <div className={cls.ThemeSwitcher__toggle}>
+        <div>
+            <div>
                 <Button theme={ThemeButton.CLEAR} onClick={toggleTheme}>
                     {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
                 </Button>
