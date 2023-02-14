d05d11576d7c4e3a866b2ae4c133d455dff04055
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2023-02-14 23:07:14 +0500

    add jest, add first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional classes', () => {
+        expect(classNames('someClass', {}, ['additionalClass'])).toBe('someClass additionalClass');
+    });
+
+    test('with mods', () => {
+        expect(classNames('someClass', { someMod: false })).toBe('someClass');
+    });
+
+    test('with mods and additional classes', () => {
+        expect(classNames('someClass', { someMod: false }, ['additionalClass'])).toBe('someClass additionalClass');
+    });
+
+    test('with mods false', () => {
+        expect(classNames('someClass', { someMod: false })).toBe('someClass');
+    });
+});
